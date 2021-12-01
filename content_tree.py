d0d14ee1852b5ef6d44c7f44b159d9b8540f74ea
Ylin <yalin20210411@outlook.com>
2021-12-01 19:55:50 +0800

    remove content_tree.py

--- content_tree.py
+++
@@ -1,8 +0,0 @@
-import typing
-from PyQt5.QtWidgets import QTreeWidget, QWidget
-
-class TOC(QTreeWidget):
-    """目录树"""
-    def __init__(self, parent: typing.Optional[QWidget] = ...) -> None:
-        super().__init__(parent=parent)
-    
